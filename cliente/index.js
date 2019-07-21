706603413c0ba19d788dea83d331d510c7eca105
Lou80 <lourdesramirez@yahoo.com>
2019-07-20 22:58:06 -0300

    carga de nuevo empleado

--- cliente/index.js
+++ cliente/index.js
@@ -27,7 +27,54 @@ fetch(`${baseURL}/api/users`)
                         style="color: #f44336"></i></a>
             </td>
         </tr>`;
-        const table = document.querySelector ('.table-wrapper table');
-        table.innerHTML += user;
+            const table = document.querySelector('.table-wrapper table');
+            table.innerHTML += user;
         })
     })
+
+document.querySelector('.modal-content form').onsubmit = function (e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const address = document.getElementById('address').value;
+    const phone = document.getElementById('phone').value;
+    const newEmployee = {
+        name: name,
+        email: email,
+        address: address,
+        phoneNumber: phone,
+    }
+    fetch(`${baseURL}/api/users`, {
+        method: 'post',
+        body: JSON.stringify(newEmployee),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+        .then(u => {
+            const user = `
+            <tr id=${u.id}>
+            <td>
+                <span class="custom-checkbox">
+                    <input type="checkbox" id="checkbox5" name="options[]" value="1">
+                    <label for="checkbox5"></label>
+                </span>
+            </td>
+            <td>${u.name}</td>
+            <td>${u.email}</td>
+            <td>${u.address}</td>
+            <td>${u.phoneNumber}</td>
+            <td>
+                <a onclick="edit(1)" href="#editEmployeeModal" class="edit" data-toggle="modal"><i
+                        class="material-icons" data-toggle="tooltip" title="Edit"
+                        style="color: #ffc107"></i></a>
+                <a onclick="remove(1)" href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i
+                        class="material-icons" data-toggle="tooltip" title="Delete"
+                        style="color: #f44336"></i></a>
+            </td>
+        </tr>`;
+            const table = document.querySelector('.table-wrapper table');
+            table.innerHTML += user;
+        });
+}
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-app.use( cors() );
-app.use( express.json() );
+app.use(cors());
+app.use(express.json());
+
+let userId = 1;
 
 const users = [
-    {id:1, name: 'caca'}
+    { id: 1, name: 'caca' }
 ];
 
 app.get('/api/users', function (req, res) {
@@ -13,7 +15,13 @@ app.get('/api/users', function (req, res) {
 });
 
 app.post('/api/users', function (req, res) {
-
+    const newEmployee = req.body;
+    /*if(newEmployee.name.length > 30 || !newEmployee.email.includes('@') || newEmployee.phoneNumber.typeOf !== 'number' ) {
+         return res.status(400).send('error');
+    }*/
+    newEmployee.id = userId++;
+    users.push(newEmployee);
+    res.json(newEmployee);
 });
 
 app.listen(3000);
