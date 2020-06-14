30005bc85c3a6196d429a7e7493fcf49b7f0d4bb
Lourdes <lourdesramirez24@gmail.com>
2020-06-13 21:49:43 -0300

    added some style, and delete request

--- cliente/index.js
+++ cliente/index.js
@@ -1,19 +1,14 @@
-const baseURL = 'http://localhost:3000';
+const baseURL = "http://localhost:3000";
 
 fetch(`${baseURL}/api/users`)
-    .then(function (res) {
-        return res.json()
-    })
-    .then(function (users) {
-        users.forEach(u => {
-            const user = `
-            <tr id=${u.id}">
-            <td>
-                <span class="custom-checkbox">
-                    <input type="checkbox" id="checkbox5" name="options[]" value="1">
-                    <label for="checkbox5"></label>
-                </span>
-            </td>
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (users) {
+    console.log(users);
+    users.forEach((u) => {
+      const user = `
+      <tr id="row_${u.id}">
             <td>${u.name}</td>
             <td>${u.email}</td>
             <td>${u.address}</td>
@@ -22,51 +17,44 @@ fetch(`${baseURL}/api/users`)
                 <a onclick="edit(1)" href="#editEmployeeModal" class="edit" data-toggle="modal"><i
                         class="material-icons" data-toggle="tooltip" title="Edit"
                         style="color: #ffc107"></i></a>
-                <a onclick="remove(1)" href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i
+                <a onclick="remove()"  class="delete" href="#removeEmployee" ><i id="${u.id}"
                         class="material-icons" data-toggle="tooltip" title="Delete"
                         style="color: #f44336"></i></a>
             </td>
         </tr>`;
-            const tableBody = document.querySelector('table tbody');
-            tableBody.innerHTML += user;
-        })
+      const tableBody = document.querySelector("table tbody");
+      tableBody.innerHTML += user;
+    });
+  });
+
+document.querySelector(".modal-content form").onsubmit = function (e) {
+  e.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const address = document.getElementById("address").value;
+  const phoneNumber = parseInt(document.getElementById("phone").value);
+  const phoneNumberOk = typeof phoneNumber;
+
+  if (name.length <= 30 && email.includes("@") && phoneNumberOk === "number") {
+    const newEmployee = {
+      name: name,
+      email: email,
+      address: address,
+      phoneNumber: phoneNumber,
+    };
+
+    fetch(`${baseURL}/api/users`, {
+      method: "post",
+      body: JSON.stringify(newEmployee),
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-
-document.querySelector('.modal-content form').onsubmit = function (e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const address = document.getElementById('address').value;
-    const phoneNumber = parseInt(document.getElementById('phone').value);
-    const phoneNumberOk = typeof phoneNumber;
-    console.log(phoneNumber, phoneNumberOk)
-
-    if (name.length <= 30 && email.includes('@') && phoneNumberOk === 'number') {
-
-        const newEmployee = {
-            name: name,
-            email: email,
-            address: address,
-            phoneNumber: phoneNumber,
-        }
-
-        fetch(`${baseURL}/api/users`, {
-            method: 'post',
-            body: JSON.stringify(newEmployee),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-            .then(res => res.json())
-            .then(u => {
-                const user = `
-            <tr id=${u.id}>
-            <td>
-                <span class="custom-checkbox">
-                    <input type="checkbox" id="checkbox5" name="options[]" value="1">
-                    <label for="checkbox5"></label>
-                </span>
-            </td>
+      .then((res) => res.json())
+      .then((u) => {
+        console.log(u);
+        const user = `
+            <tr id="row_${u.id}">
             <td>${u.name}</td>
             <td>${u.email}</td>
             <td>${u.address}</td>
@@ -75,15 +63,29 @@ document.querySelector('.modal-content form').onsubmit = function (e) {
                 <a onclick="edit(1)" href="#editEmployeeModal" class="edit" data-toggle="modal"><i
                         class="material-icons" data-toggle="tooltip" title="Edit"
                         style="color: #ffc107"></i></a>
-                <a onclick="remove(1)" href="#deleteEmployeeModal" class="delete" data-toggle="modal"><i
+                <a onclick="remove()"  class="delete" href="#editEmployeeModal"  ><i id="${u.id}"
                         class="material-icons" data-toggle="tooltip" title="Delete"
                         style="color: #f44336"></i></a>
             </td>
         </tr>`;
-                const tableBody = document.querySelector('table tbody');
-                tableBody.innerHTML += user;
-            });
-    } else {
-        console.log('hay algún error');
-    }
-}
+        const tableBody = document.querySelector("table tbody");
+        tableBody.innerHTML += user;
+      });
+  } else {
+    console.log("hay algún error");
+  }
+};
+
+const remove = () => {
+  const employeeId = event.target.id;
+
+  fetch(`${baseURL}/api/users/${employeeId}`, {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((res) => {
+    const targetRow = document.getElementById(`row_${employeeId}`);
+    res.ok ? targetRow.parentNode.removeChild(targetRow) : console.log("error");
+  });
+};
--- server.js
+++ server.js
@@ -1,29 +1,45 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-let userId = 1;
+let userId = 2;
 
-const users = [];
+const users = [
+  {
+    id: 13,
+    name: "Juan Carlos Batman",
+    email: "juan.carlos@batman.com",
+    address: "Pangolin 345, Wuhan",
+    phoneNumber: 78963014,
+  },
+];
 
-app.get('/api/users', function (req, res) {
-    res.json(users)
+app.get("/api/users", function (req, res) {
+  res.json(users);
 });
 
-app.post('/api/users', function (req, res) {
-    const newEmployee = req.body;
-    const phone = newEmployee.phoneNumber;
-    const phoneOk = typeof phone;
-    
-    if (newEmployee.name.length > 30 || !newEmployee.email.includes('@') || phoneOk !== 'number') {
-        console.log(phone, phoneOk);
-        return res.status(400).send('error');
-    }
-    newEmployee.id = userId++;
-    users.push(newEmployee);
-    res.json(newEmployee);
+app.post("/api/users", function (req, res) {
+  const newEmployee = req.body;
+  const phone = newEmployee.phoneNumber;
+  const phoneOk = typeof phone;
+
+  if (
+    newEmployee.name.length > 30 ||
+    !newEmployee.email.includes("@") ||
+    phoneOk !== "number"
+  ) {
+    return res.status(400).send("error");
+  }
+  newEmployee.id = userId++;
+  users.push(newEmployee);
+  res.json(newEmployee);
+});
+
+app.delete("/api/users/:id", function (req, res) {
+  res.json();
+  //console.log(req.params.id);
 });
 
 app.listen(3000);
